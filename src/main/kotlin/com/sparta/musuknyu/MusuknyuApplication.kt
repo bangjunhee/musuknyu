410d0a100146daf930d935bb933e685b31af29d7
Bang <threejun1007@gmail.com>
2024-02-14 19:37:25 +0900

    init commit

---
+++ src/main/kotlin/com/sparta/musuknyu/MusuknyuApplication.kt
@@ -0,0 +1,11 @@
+package com.sparta.musuknyu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusuknyuApplication
+
+fun main(args: Array<String>) {
+	runApplication<MusuknyuApplication>(*args)
+}
